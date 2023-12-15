4a8e878993479b7d7cb011ac256e9c61ecda1d12
git2andi <andreas.schwab2020@gmx.de>
2023-12-15 17:55:45 +0100

    redo score

--- getScore.py
+++ getScore.py
@@ -3,20 +3,25 @@ import clip
 from PIL import Image
 import argparse
 
-parser = argparse.ArgumentParser(description="Image Path for CLIP")
-parser.add_argument("--path", type=str, help="Path to the image file")
-parser.add_argument("--prompt", type=str, help="Text prompt for CLIP")
+# Define command-line arguments
+parser = argparse.ArgumentParser(description="Image Path and Prompt for CLIP")
+parser.add_argument("--path", help="Path to the image file")
+parser.add_argument("--prompt", nargs='+', help="Text prompts for CLIP")
 args = parser.parse_args()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 image_path = args.path
-prompt = args.prompt
+prompts = args.prompt
 
+# Print image path and prompts for verification
+print("Image path:", image_path)
+print("Prompts:", prompts)
+
+# Load and preprocess the image
 image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-text = clip.tokenize([prompt]).to(device)
-
+text = clip.tokenize(prompts).to(device)
 
 with torch.no_grad():
     # Encode the image and text
@@ -27,5 +32,5 @@ with torch.no_grad():
     logits_per_image, logits_per_text = model(image, text)
     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
-# Print the label probabilities
-print("Label probabilities:", probs)
+# Print the label probabilities in decimal form
+print("Label probabilities:", probs.tolist())
